d48177fade8f00ba5257baad1c0acea3cdd07c5d
arbib-adil <arbibadilv2@gmail.com>
2022-11-04 14:06:49 +0100

    adding helper.jar file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.team.restaurant_admin_panel {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires helper;
+
     opens com.team.restaurant_admin_panel to javafx.fxml;
     opens com.team.restaurant_admin_panel.controllers to javafx.fxml;
     exports com.team.restaurant_admin_panel;
